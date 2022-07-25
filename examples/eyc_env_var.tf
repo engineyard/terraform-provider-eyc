89f2e18ce74fd59fa8ad0645372a1736061cf421
Kyle-Law <kylelaw1121@gmail.com>
2022-07-25 14:45:05 +0800

    Update TF examples

--- examples/eyc_env_var.tf
+++ examples/eyc_env_var.tf
@@ -3,6 +3,6 @@ resource "eyc_env_var" "env1" {
 
   app_id = var.env_var_of_env1[count.index]["app_id"]
   env_id = var.env_var_of_env1[count.index]["env_id"]
-  key    = var.env_var_of_env1[count.index]["key"]
+  name    = var.env_var_of_env1[count.index]["name"]
   value  = var.env_var_of_env1[count.index]["value"]
 }
